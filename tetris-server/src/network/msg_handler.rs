b5d6f48017de6d4c259f90234d405fdc17a7e076
Jonathan Louie <jonathanrlouie@gmail.com>
2018-01-08 00:13:43 -0800

    Just started building out MessageHandler

--- tetris-server/src/network/msg_handler.rs
+++ tetris-server/src/network/msg_handler.rs
@@ -1,5 +1,20 @@
 use amethyst::ecs::{Component, DenseVecStorage};
 
-struct MessageHandler {
-    
+pub struct MessageHandler {
+
+}
+
+impl Component for MessageHandler {
+    type Storage = DenseVecStorage<Self>;
+}
+
+impl MessageHandler {
+    pub fn new() -> MessageHandler {
+        MessageHandler {}
+    }
+
+    pub fn recv<T>(&self) -> T {
+
+
+    }
 }
